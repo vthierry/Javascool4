3d1ffe4e575423f3922691dae88e85b7742eda0c
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-07-04 20:03:13 +0000

    correction bug pml + ajout fichiers textes
    git-svn-id: http://javascool.googlecode.com/svn@52 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- trunk/src/org/javascool/tests/PmlTst.java
+++ trunk/src/org/javascool/tests/PmlTst.java
@@ -1,6 +1,11 @@
 import org.javascool.Pml;
 
+/** Read/write test of Pml data structures. */
 public class PmlTst {
+  /** Runs the test.
+     * * Usage: <tt>java org.javascool.test.PmlTst</tt>
+     * @param argv Not used.
+     */
   public static void main(String argv[]) {
     tst("{test coco=titi toto}", "pml");
     tst("{test coco=titi toto=} ", "pml");
@@ -8,7 +13,8 @@ public class PmlTst {
     tst("<root><toto>ABCD</toto></root>", "pml");
     tst("<root><toto>ABCD</toto><toto>ADER</toto></root>", "xml");
   }
-  public static void tst(String string, String format) {
+  // Read and then write in normalized format
+  private static void tst(String string, String format) {
     System.out.println("in>"+string);
     Pml pml = new Pml().reset(string, format);
     System.out.println("out>"+pml);
