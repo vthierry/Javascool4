3fa7cd5cc9222e90c62a96b6fc5b83e3fbad8f80
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-07-18 12:44:05 +0000

    mise en place du passage des proglets en 3.3
    git-svn-id: http://javascool.googlecode.com/svn@140 17e3714e-118d-3e7a-01ef-bee73ecf6174

---
+++ trunk/sketchbook/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <title>Proglets proposées sur le site de JavaScool</title>
+  </head>
+
+  <body>
+    <h1>Proglets proposées sur le site de JavaScool</h1>
+    
+    <?php /* Ici sera mis le code PHP qui permet de fabriqué la table des proglets disponibles. */ ?>
+
+  </body>
+</html>
