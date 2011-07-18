565acace9261e75e95d2c5e08703d624ef2464c1
guillaume.quest <guillaume.quest@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-07-18 08:08:52 +0000

    SketchbookCompiler
    git-svn-id: http://javascool.googlecode.com/svn@122 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- trunk/src/org/javascool/tools/SketchbookCompiler.java
+++ trunk/src/org/javascool/tools/SketchbookCompiler.java
@@ -5,10 +5,15 @@
  */
 package org.javascool.tools;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -31,45 +36,139 @@ public class SketchbookCompiler {
     private String jvsPath = "";
     private Boolean isInJar = true;
 
+    /**
+    Remove a directory and all of its contents.
+    
+    The results of executing File.delete() on a File object
+    that represents a directory seems to be platform
+    dependent. This method removes the directory
+    and all of its contents.
+    
+    @return true if the complete directory was removed, false if it could not be.
+    If false is returned then some of the files in the directory may have been removed.
+    
+     */
+    public static boolean removeDirectory(File directory) {
+        if (directory == null) {
+            return false;
+        }
+        if (!directory.exists()) {
+            return true;
+        }
+        if (!directory.isDirectory()) {
+            return false;
+        }
+
+        String[] list = directory.list();
+
+        // Some JVMs return null for File.list() when the
+        // directory is empty.
+        if (list != null) {
+            for (int i = 0; i < list.length; i++) {
+                File entry = new File(directory, list[i]);
+                if (entry.isDirectory()) {
+                    if (!removeDirectory(entry)) {
+                        return false;
+                    }
+                } else {
+                    if (!entry.delete()) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return directory.delete();
+    }
+
+    //TODO javadoc
+    private static void copyfile(String srFile, String dtFile) {
+        try {
+            File f2 = new File(dtFile);
+            InputStream in = ClassLoader.getSystemResourceAsStream(srFile);
+            
+            OutputStream out = new FileOutputStream(f2);
+
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage() + " in the specified directory. Error on file "+srFile);
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
     public SketchbookCompiler() throws IOException {
-        
         // List files in the path
         try {
             if (ClassLoader.getSystemClassLoader().getResource("org/javascool").toString().split("!").length > 1) {
-                this.jvsPath = ClassLoader.getSystemClassLoader().getResource("org").toString().replaceAll("file:", "").replaceAll("jar:", "").replaceAll("%20", " ").split("!")[0];
+                this.jvsPath = ClassLoader.getSystemClassLoader().getResource("org/javascool/").toString().replaceAll("file:", "").replaceAll("jar:", "").replaceAll("%20", " ").split("!")[0];
                 this.isInJar = true;
                 this.files = this.listFilesInJar(this.jvsPath);
             } else {
-                this.jvsPath = ClassLoader.getSystemClassLoader().getResource("").toURI().getPath();
+                this.jvsPath = ClassLoader.getSystemClassLoader().getResource("org/javascool/").toURI().getPath();
                 this.isInJar = false;
                 this.files = this.listFilesInPath(this.jvsPath);
             }
         } catch (URISyntaxException uRISyntaxException) {
             System.err.println(uRISyntaxException.getMessage());
         }
-        
-        // Create a tmp directory
-        File tmpDir=this.getTmpDir();
-        tmpDir.mkdirs();
-        
-        
-        // These are the files to include in the ZIP file
-        String[] filenames = new String[]{"build.xml", "logos/logo.png"};
 
+        // Create a tmp directory
+        File tmpDir = this.getTmpDir();
+        System.out.println(tmpDir);
+        tmpDir.mkdirs();
+
+
+        // These are the files to include in the ZIP file
+        ArrayList<String> filenames = new ArrayList<String>();
+        ArrayList<String> filenamesJar = new ArrayList<String>();
+
+        for (String file : this.files) {
+            if (file.endsWith("/")) {
+                File dir = new File(tmpDir + "/" + file);
+                dir.mkdirs();
+            } else if (!file.contains("META-INF")) {
+                File dest = new File(tmpDir + "/" + file);
+                copyfile(file, dest.getAbsolutePath());
+                filenames.add(dest.getAbsolutePath());
+                filenamesJar.add(file);
+            }
+            System.out.println("Copied " + file);
+        }
+        
+        filenames.add((new File(tmpDir+"/META-INF/MANIFEST.MF")).getAbsolutePath());
+        filenamesJar.add("META-INF/MANIFEST.MF");
+        String manifest = new String();
+        manifest += "Manifest-Version: 1.0\n";
+        manifest += "Created-By: INRIA\n";
+        manifest += "Main-Class: org.javascool.JvsMain\n";
+        FileWriter fstream=new FileWriter(tmpDir+"/META-INF/MANIFEST.MF");
+        BufferedWriter outs=new BufferedWriter(fstream);
+        outs.write(manifest);
+        outs.close();
+        
         // Create a buffer for reading the files
         byte[] buf = new byte[1024];
 
         try {
             // Create the ZIP file
-            String outFilename = "outfile.zip";
+            String outFilename = "outfile.jar";
             ZipOutputStream out = new ZipOutputStream(new FileOutputStream(outFilename));
 
             // Compress the files
-            for (int i = 0; i < filenames.length; i++) {
-                FileInputStream in = new FileInputStream(filenames[i]);
+            for (int i = 0; i < filenames.size(); i++) {
+                FileInputStream in = new FileInputStream(filenames.get(i));
 
                 // Add ZIP entry to output stream.
-                out.putNextEntry(new ZipEntry(filenames[i]));
+                out.putNextEntry(new ZipEntry(filenamesJar.get(i)));   //FIXME
 
                 // Transfer bytes from the file to the ZIP file
                 int len;
@@ -85,8 +184,11 @@ public class SketchbookCompiler {
             // Complete the ZIP file
             out.close();
         } catch (IOException e) {
+            e.printStackTrace();
             System.out.println(e);
         }
+        
+        System.out.println("Jar creation completed");
     }
 
     private ArrayList<String> listFilesInPath(String path) {
@@ -104,7 +206,7 @@ public class SketchbookCompiler {
     private ArrayList<String> listFilesInJar(String jarPath) {
         ArrayList<String> jarFiles = new ArrayList<String>();
         try {
-            System.out.println("jar : "+jarPath);
+            System.out.println("jar : " + jarPath);
             JarFile jarFile = new JarFile(jarPath);
             final Enumeration<JarEntry> entries = jarFile.entries();
             while (entries.hasMoreElements()) {
@@ -117,7 +219,7 @@ public class SketchbookCompiler {
         }
         return jarFiles;
     }
-    
+
     private File getTmpDir() {
         String OS = System.getProperty("os.name").toUpperCase();
         if (OS.contains("WIN")) {
@@ -128,7 +230,7 @@ public class SketchbookCompiler {
         } else if (OS.contains("NUX")) {
             return new File(System.getProperty("user.home") + "/.javascool/tmp/");
         }
-        return new File(System.getProperty("user.dir")+"/.javascool/tmp/");
+        return new File(System.getProperty("user.dir") + "/.javascool/tmp/");
     }
 
     public static void main(String[] args) {
