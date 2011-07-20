6c0e9303c25b9a4857ea931f347ae0898432eb32
guillaume.quest <guillaume.quest@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-07-20 08:01:09 +0000

    sources, etc...
    git-svn-id: http://javascool.googlecode.com/svn@163 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- web/pages/contact/display.php
+++ web/pages/contact/display.php
@@ -1,11 +1,11 @@
 <table><tr><td><span class="labelclickable" onclick="gotoloc('index.php');">Java's Cool</span><span class="label-arrow"></span><span class="label">Contact</span></td></tr></table><br />
 
-<p>Java's Cool est d&eacute;velopp&eacute; par des membres de l'Institur National de Recherche en Informatique et Automatique (INRIA) &agrave; Sophia Antipolis (06).</p>
+<p>Java's Cool est d&eacute;velopp&eacute; par des membres de l'Institut National de Recherche en Informatique et Automatique (INRIA) &agrave; Sophia Antipolis (06).</p>
 <div class="label2">L'&eacute;quipe de d&eacute;veloppement</div>
 <ul class="developpers">
-<li>Thierry Vi&eacute;ville (thierry@vieville.fr)</li>
-<li>Philippe Vienne (philippe@vienne.fr)</li>
-<li>Guillaume Matheron (guillaume@matheron.fr)</li>
+    <li>Thierry Vi&eacute;ville (thierry@vieville.fr)</li>
+    <li>Philippe Vienne (philippe@vienne.fr)</li>
+    <li>Guillaume Matheron (guillaume@matheron.fr)</li>
 </ul>
 <div class="label2">Soumettre un bug ou proposer une fonctionnalit&eacute;</div>
 <p>Vous pouvez trouver notre syst&egrave;me de gestion des t&acirc;ches &agrave; l'adresse suivante : <a href="http://code.google.com/p/javascool/issues/list">http://code.google.com/p/javascool/issues/list</a></p>
@@ -13,3 +13,9 @@
 </div>
 <div class="label2">Nous conter par t&eacute;l&eacute;phone</div>
 <p>Vous pouvez nous contacter au num&eacute;ro suivant : 00 00 00 00 00</p>
+
+<img src="images/logo_INRIA.png" alt="INRIA" onClick="gotoloc('inria.fr')" class="imagelink"/>
+<img src="images/logo_unisciel.png" alt="Unisciel" onClick="gotoloc('unisciel.fr')" class="imagelink"/>
+<img src="images/logo_fuscia.png" alt="Fuscia" onClick="gotoloc('fuscia.info')" class="imagelink"/>
+<img src="images/logo_linuxazur.png" alt="Linux Azur" onClick="gotoloc('linux-azur.org')" class="imagelink"/>
+<img src="images/logo_unice.gif" alt="INRIA" onClick="gotoloc('unice.fr')" class="imagelink"/>
