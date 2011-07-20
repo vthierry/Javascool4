c229205fc9a8e185f44ef0fe2aea8dea5d703a7c
guillaume.quest <guillaume.quest@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-07-20 10:26:51 +0000

    js and anims
    git-svn-id: http://javascool.googlecode.com/svn@171 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- web/pages/header.php
+++ web/pages/header.php
@@ -2,11 +2,11 @@
 <td class="menu-left"></td>
 <td class="logo"><a href="index.php"><img class="logo" src="pages/logo.png" alt="Java's Cool"/></a></td>
 
-<td onclick="gotoloc('index.php')" class="<?php echo(($page=="home")?"menuitemselected":"menuitem"); ?>">Accueil</td>
-<td onclick="gotoloc('index.php?page=run')" class="<?php echo(($page=="run")?"menuitemselected":"menuitem"); ?>">Lancer</td>
-<td onclick="gotoloc('index.php?page=proglets')" class="<?php echo(($page=="proglets")?"menuitemselected":"menuitem"); ?>">Proglets</td>
-<td onclick="gotoloc('index.php?page=resources')" class="<?php echo(($page=="resources")?"menuitemselected":"menuitem"); ?>">Ressources</td>
-<td onclick="gotoloc('index.php?page=developers')" class="<?php echo(($page=="developers")?"menuitemselected":"menuitem"); ?>">D&eacute;veloppeurs</td>
-<td onclick="gotoloc('index.php?page=contact')" class="<?php echo(($page=="contact")?"menuitemselected":"menuitem"); ?>">Contact</td>
+<td id="menu1_l" class="menu<?php echo(($page=="home")?"selected":""); ?>begin"></td><td id="menu1" onclick="gotoloc('index.php')" class="<?php echo(($page=="home")?"menuitemselected":"menuitem"); ?>">Accueil</td><td class="menu<?php echo(($page=="home")?"selected":""); ?>end" id="menu1_r">
+<td id="menu2_l" class="menu<?php echo(($page=="run")?"selected":""); ?>begin"><td id="menu2" onclick="gotoloc('index.php?page=run')" class="<?php echo(($page=="run")?"menuitemselected":"menuitem"); ?>">Lancer</td><td class="menu<?php echo(($page=="run")?"selected":""); ?>end" id="menu2_r">
+<td id="menu3_l" class="menu<?php echo(($page=="proglets")?"selected":""); ?>begin"><td id="menu3" onclick="gotoloc('index.php?page=proglets')" class="<?php echo(($page=="proglets")?"menuitemselected":"menuitem"); ?>">Proglets</td><td class="menu<?php echo(($page=="proglets")?"selected":""); ?>end" id="menu3_r">
+<td id="menu4_l" class="menu<?php echo(($page=="resources")?"selected":""); ?>begin"><td id="menu4" onclick="gotoloc('index.php?page=resources')" class="<?php echo(($page=="resources")?"menuitemselected":"menuitem"); ?>">Ressources</td><td class="menu<?php echo(($page=="resources")?"selected":""); ?>end" id="menu4_r">
+<td id="menu5_l" class="menu<?php echo(($page=="developers")?"selected":""); ?>begin"><td id="menu5" onclick="gotoloc('index.php?page=developers')" class="<?php echo(($page=="developers")?"menuitemselected":"menuitem"); ?>">D&eacute;veloppeurs</td><td class="menu<?php echo(($page=="developers")?"selected":""); ?>end" id="menu5_r">
+<td id="menu6_l" class="menu<?php echo(($page=="contact")?"selected":""); ?>begin"><td id="menu6" onclick="gotoloc('index.php?page=contact')" class="<?php echo(($page=="contact")?"menuitemselected":"menuitem"); ?>">Contact</td><td class="menu<?php echo(($page=="contact")?"selected":""); ?>end" id="menu6_r">
 
 <td class="menu-right"></td></tr></table>
