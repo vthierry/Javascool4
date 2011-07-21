157ed2da11202884956d23d83b08654c77d2e926
guillaume.quest <guillaume.quest@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-07-21 11:41:44 +0000

    website - downloaded contents from wiki
    git-svn-id: http://javascool.googlecode.com/svn@197 17e3714e-118d-3e7a-01ef-bee73ecf6174

---
+++ web/pages/resources/autresdeveloppements.php
@@ -0,0 +1,27 @@
+<p><br />
+<b>Culture Scientifique&nbsp;: ateliers logiciels pour des réalisations culturelles ou pédagogiques.</b>
+</p><p>En partenariat avec le <a href="http://deptinfo.unice.f" class="external text" rel="nofollow">Master d'Informatique de l'Université de Nice-Sophia</a>, les enseignants des lycées partenaires sollicitent des groupes d'étudiants dans le cadre de leur projet de fin d'étude pour réaliser des logiciels à vocation pédagogique. Le projet est proposé par des professeurs et réalisé par des étudiants en informatique et co-encadré par un chercheur spécialiste du sujet. <br /><br /> Par ailleurs, dans le cadre d'un autre type de colloboration, la "<a href="http://www.dessi.fr" class="external text" rel="nofollow">Junior Entreprise</a>" accepte, à des tarifs compatibles avec les budgets publics, de réaliser plus ponctuellement des documents multi-média interactifs de culture scientifique tels que le <a href="http://interstices.info/tatouage-son" class="external text" rel="nofollow">tatouage de son</a>, les <a href="http://interstices.info/algorithme-geometrique" class="external text" rel="nofollow">algorithmes géométriques</a>, les <a href="http://interstices.info/araignee" class="external text" rel="nofollow">modèles bio-inspirés</a>. De même, de jeunes ingénieur(e)s de l'Ecole d'Ingénieur du campus STIC, <a href="http://www.polytech.unice.fr" class="external text" rel="nofollow">SophiaTech</a>, ont réalisé des "gamelets" permettant d'illustrer d'autres documents multi-média (par exemple sur la <a href="http://interstices.info/display.jsp?id=c_14155" class="external text" rel="nofollow">reconnaissance de formes</a>, la <a href="http://interstices.info/display.jsp?id=c_14151" class="external text" rel="nofollow">génération de trajectoires</a>, l'<a href="http://interstices.info/display.jsp?id=c_14161" class="external text" rel="nofollow">estimation paramétrique</a>).
+</p><p>Deux projets sont aboutis&nbsp;:
+</p>
+<ul><li> <a href="http://javascool.gforge.inria.fr/documents/poster-atelier-js-proglet.pdf" class="external text" rel="nofollow">Environnement de programmation pédagogique pour le lycée</a>, (apprentissage de la programmation, mesures numériques, ...) avec Laurent Mongiat, professeur au lycée régional de Valbonne, avec les conseils de <a href="http://www-sop.inria.fr/oasis/personnel/Fabrice.Huet" class="external text" rel="nofollow">Fabrice Huet</a>, enseignant-chercheur de l'Université de Nice-Sophia.&lt;/span&gt; et le <a href="http://deptinfo.unice.fr/%7Ehuet/M1INFO/07-08/TER/mongiat.html" class="external text" rel="nofollow">Master Informatique de Nice</a>.
+</li></ul>
+<p>En savoir plus sur le logicel <a href="http://javascool.gforge.inria.fr" class="external text" rel="nofollow">JavaScool</a>
+</p>
+<ul><li> <a href="http://www-sop.inria.fr/culture_scient/documents/poster-rdmschool.pdf" class="external text" rel="nofollow">Simulation et optimisation d'un pont en fil de fer et en nylon</a>, (simulation de calculs mécaniques, logiciels numériques intégrés, ...) avec Walter Lattanzio, professeur au lycée <a href="http://leonarddevinci.net" class="external text" rel="nofollow">Léonard de Vinci</a>, d'Antibes avec les conseils de <a href="http://www.ensmp.fr/cgi-bin/whoswhoTC?Jserver=ph.ensmp.fr&amp;Qid=6969" class="external text" rel="nofollow">Marc Bernacki</a> chercheur à l'Ecole des Mines de Paris, site de Sophia et le <a href="http://deptinfo.unice.fr/%7Ehuet/M1INFO/07-08/TER/vieville.html" class="external text" rel="nofollow">Master Informatique de Nice</a>.
+</li></ul>
+<p>En savoir plus sur le logiciel <a href="http://rdmschool.gforge.inria.fr" class="external text" rel="nofollow">RdmSchool</a>
+</p><p>Ces deux logiciels font l'objet d'un dépot APP sous une licence <a href="http://www.cecill.info" class="external text" rel="nofollow">CeCILL,</a> les étudiants et professeurs des lycées étant co-auteurs de ces logiciels libres.
+</p><p>Nous vous proposons aussi&nbsp;:
+</p>
+<ul><li> [culture_scient/documents/poster-mobinet.pdf MobiNet], qui est un logiciel permettant de programmer le comportement d'objets mobiles animés à l'aide d'une interface et d'un langage intuitifs. <a href="http://mobinet.imag.fr" class="external text" rel="nofollow">MobiNet</a> est totalement interactif, facilitant apprentissage et mise au point des mobiles. En outre, <a href="http://mobinet.imag.fr" class="external text" rel="nofollow">MobiNet</a> est conçu pour fonctionner en réseau (mais il est également utilisable sur un poste isolé). Il a été réalisé sous la responsabilité de <a href="http://evasion.inrialpes.fr/Membres/Fabrice.Neyret" class="external text" rel="nofollow">Fabrice Neyret,</a> chercheur à Grenoble.En savoir plus sur le logiciel <a href="http://mobinet.imag.fr" class="external text" rel="nofollow">MobiNet</a>
+</li></ul>
+
+<!-- 
+NewPP limit report
+Preprocessor node count: 3/1000000
+Post-expand include size: 0/2097152 bytes
+Template argument size: 0/2097152 bytes
+Expensive parser function count: 0/100
+-->
+
+<!-- Saved in parser cache with key wiki_sciencinfolycee:pcache:idhash:114-1!1!0!!fr!2!edit=0!printable=1 and timestamp 20110721112742 -->
