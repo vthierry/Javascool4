3466d06fb2ee04d9ca33770c952f932c6ee0e753
guillaume.quest@gmail.com <guillaume.quest@gmail.com@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-07-25 13:40:04 +0000

    Added Jeux2D to sketchbook
    git-svn-id: http://javascool.googlecode.com/svn@239 17e3714e-118d-3e7a-01ef-bee73ecf6174

---
+++ web/sketchbook/jeux2D/StoredProperty.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.javascool.proglets.game;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author gmatheron
+ */
+public class StoredProperty {
+    private String m_name;
+    private Object m_value;
+    private Class<?> m_type;
+    
+    public StoredProperty() {
+        m_name="";
+        m_value=null;
+    }
+    public StoredProperty(String s, Object o, Class<?> c) {
+        m_name=s;
+        m_value=o;
+        m_type=c;
+    }
+    public boolean isNull() {
+        return (m_value==null);
+    }
+    public String getName() {
+        return (m_name);
+    }
+    public Object getObject() {
+        return (m_value);
+    }
+    public Class getType() {
+        return (m_type);
+    }
+    public void setObject(Object o) {
+        m_value=o;
+    }
+    
+    private static final Logger LOG = Logger.getLogger(StoredProperty.class.getName());
+}
