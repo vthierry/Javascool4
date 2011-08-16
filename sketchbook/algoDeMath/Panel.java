582597d24ea4df89f40c0518941f319bd14fcf4c
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-08-16 08:40:42 +0000

    git-svn-id: http://javascool.googlecode.com/svn@398 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- sketchbook/algoDeMath/Panel.java
+++ sketchbook/algoDeMath/Panel.java
@@ -1,15 +1,41 @@
 package org.javascool.proglets.algoDeMath;
-import javax.swing.JApplet;
+import static org.javascool.tools.Macros.*;
+import static org.javascool.proglets.algoDeMath.Functions.*;
 
-public class Panel extends JApplet {
-/** Définition de l'interface graphique de la proglet. */
-  public static ProgletPanel panel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import org.javascool.widgets.CurveOutput;
+import org.javascool.widgets.NumberInput;
 
-  public Panel() {
-    panel = new ProgletPanel();
+public class Panel extends JPanel {
+  private static final long serialVersionUID = 1L;
+
+  // @bean
+  public ProgletPanel() {
+    super(new BorderLayout());
+    add(scope = new CurveOutput(), BorderLayout.CENTER);
+    JPanel input = new JPanel(new BorderLayout());
+    input.add(inputX = new NumberInput("X"), BorderLayout.NORTH);
+    input.add(inputY = new NumberInput("Y"), BorderLayout.SOUTH);
+    Runnable run1 = new Runnable() {
+      public void run() {
+        inputX.setValue(scope.getReticuleX());
+        inputY.setValue(scope.getReticuleY());
+      }
+    };
+    Runnable run2 = new Runnable() {
+      public void run() {
+        scope.setReticule(inputX.getValue(), inputY.getValue());
+      }
+    };
+    scope.setRunnable(run1);
+    inputX.setRunnable(run2);
+    inputY.setRunnable(run2);
+    add(input, BorderLayout.SOUTH);
+    reset(1, 1);
   }
-  public void start() {}
-  public void init() {}
-  public void destroy() {}
-  public void stop() {}
+  /** Tracé de courbes. */
+  public CurveOutput scope;
+  /** Entrées de valeurs numériques. */
+  public NumberInput inputX, inputY;
 }
