88c302f35b93f6c1d44d30f114190dba905331df
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-08-23 16:50:44 +0000

    qq petites corrections mineures
    git-svn-id: http://javascool.googlecode.com/svn@561 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- work/src/org/javascool/builder/ProgletsBuilder.java
+++ work/src/org/javascool/builder/ProgletsBuilder.java
@@ -183,7 +183,8 @@ public class ProgletsBuilder {
 	  String name = new File(proglet).getName();
 	  String javascoolPrefix = "org" + File.separator + "javascool" + File.separator;
 	  String jarEntries[] = { 
-	    javascoolPrefix + "core", javascoolPrefix + "gui", javascoolPrefix + "macros", javascoolPrefix + "tools", javascoolPrefix + "widgets", 
+	    javascoolPrefix + "Core", "org" + File.separator + "fife", 
+	    javascoolPrefix + "builder", javascoolPrefix + "core", javascoolPrefix + "gui", javascoolPrefix + "macros", javascoolPrefix + "tools", javascoolPrefix + "widgets", 
 	    javascoolPrefix + "proglets" + File.separator + name};
 	  String tmpJar = buildDir + File.separator + "javascool-proglet-"+name+".jar";
 	  JarManager.jarCreate(tmpJar, buildDir + "/manifest.jmf", jarDir, jarEntries);
