7fdf19e4df32e39ca3c8bcdab25091ff96c69b97
guillaume.quest@gmail.com <guillaume.quest@gmail.com@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-08-26 08:05:30 +0000

    git-svn-id: http://javascool.googlecode.com/svn@651 17e3714e-118d-3e7a-01ef-bee73ecf6174

---
+++ sketchbook/jeuxConverted/Translator.java
@@ -0,0 +1,17 @@
+package org.javascool.proglets.jeuxConverted;
+
+/** Définit la traduction d'un code Jvs en code Java  pour manipuler la proglet «jeuxConverted» (A DÉTRUIRE SI NON UTILISÉ).
+ *
+ * @see <a href="Translator.java.html">code source</a>
+ * @serial exclude
+ */
+public class Translator extends org.javascool.core.Translator {
+    @Override
+     public String getImports() {
+    return "";
+  }
+    @Override
+  public String translate(String code) {
+    return code;
+  }
+}
