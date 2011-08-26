d27dd4a39c394def849795a1118935307126c05f
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-08-26 13:47:54 +0000

    travail sur les proglets
    git-svn-id: http://javascool.googlecode.com/svn@684 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- sketchbook/algoDeMaths/Functions.java
+++ sketchbook/algoDeMaths/Functions.java
@@ -11,10 +11,10 @@ public class Functions {
     return getProgletPane();
   }
   /** Initialise le tracé.
-   * @param Xscale Echelle maximale horizontale, l'abscisse sera tracée dans [-X, X], par défaut [-1, 1].
-   * @param Yscale Echelle maximale verticale, l'ordonnée sera tracée dans [-Y, Y], par défaut [-1, 1].
+   * @param Xscale Echelle maximale horizontale, l'abscisse sera tracée dans [-Xscale, Xscale], par défaut [-1, 1].
+   * @param Yscale Echelle maximale verticale, l'ordonnée sera tracée dans [-Yscale, Yscale], par défaut [-1, 1].
    */
-  public static void scopeReset(double Xscale, double Yscale) {
+  public static void reset(double Xscale, double Yscale) {
     getPane().inputX.setScale(-Xscale, Xscale, 0.001);
     getPane().inputY.setScale(-Yscale, Yscale, 0.001);
     getPane().scope.reset(0, 0, Xscale, Yscale);
@@ -25,46 +25,45 @@ public class Functions {
    * @param Ymin Echelle minimale verticale, l'abscisse sera tracée dans [-Ymin, Ymay], par défaut [-1, 1].
    * @param Ymax Echelle maximale verticale, l'abscisse sera tracée dans [-Ymin, Ymax], par défaut [-1, 1].
    */
-  public static void scopeReset(double Xmin, double Xmax, double Ymin, double Ymax) {
+  public static void reset(double Xmin, double Xmax, double Ymin, double Ymax) {
     getPane().inputX.setScale(Xmin, Xmax, 0.001);
     getPane().inputY.setScale(Ymin, Ymax, 0.001);
     getPane().scope.reset((Xmin + Xmax) / 2, (Ymin + Ymax) / 2, (Xmax - Xmin) / 2, (Ymax - Ymin) / 2);
   }
   /*
-   * @see #scopeReset(double, double, double, double)
+   * @see #reset(double, double, double, double)
    */
-  public static void scopeReset() {
-    scopeReset(1, 1);
+  public static void reset() {
+    reset(1, 1);
   }
   /** Change la valeur d'un point du tracé.
    * @param x Abcisse de la courbe, dans [-X, X], par défaut [-1, 1].
    * @param y Ordonnée de la courbe, dans [-Y, Y], par défaut [-1, 1].
    * @param c Numéro de la courbe: 0 (noir, défaut), 1 (brun), 2 (rouge), 3 (orange), 4 (jaune), 5 (vert), 6 (bleu), 7 (violet), 8 (gris), 9 (blanc).
    */
-  public static void scopeSet(double x, double y, int c) {
+  public static void addPoint(double x, double y, int c) {
     getPane().scope.add(x, y, c);
   }
   /*
-   * @see #scopeSet(double, double, int)
+   * @see #addPoint(double, double, int)
    */
-  public static void scopeSet(double x, double y) {
-    scopeSet(x, y, 0);
+  public static void addPoint(double x, double y) {
+    addPoint(x, y, 0);
   }
-  /** Ajoute un chaîne de caratère au tracé.
-   * <p><tt>scopeAdd</tt> est une abréviation pour <tt>scopeAddString</tt>.</p>
+  /** Ajoute une chaîne de caractères au tracé.
    * @param x Abcisse du coin inférieur gauche de la chaîne, dans [-X, X], par défaut [-1, 1].
    * @param y Ordonnée du coin inférieur gauche de la chaîne, dans [-Y, Y], par défaut [-1, 1].
    * @param s Valeur de la chaîne de caractères.
-   * @param c Couleur de la chaîne de caractères: 0 (noir, défaut), 1 (brun), 2 (rouge), 3 (orange), 4 (jaune), 5 (vert), 6 (bleu), 7 (violet), 8 (gris), 9 (blanc).
+   * @param c Couleur du point: 0 (noir, défaut), 1 (brun), 2 (rouge), 3 (orange), 4 (jaune), 5 (vert), 6 (bleu), 7 (violet), 8 (gris), 9 (blanc).
    */
-  public static void scopeAddString(double x, double y, String s, int c) {
+  public static void addString(double x, double y, String s, int c) {
     getPane().scope.add(x, y, s, c);
   }
   /*
-   * @see #scopeAddString(double, double, double, String, int)
+   * @see #addString(double, double, double, String, int)
    */
-  public static void scopeAddString(double x, double y, String s) {
-    scopeAddString(x, y, s, 0);
+  public static void addString(double x, double y, String s) {
+    addString(x, y, s, 0);
   }
   /** Trace un rectangle.
    * @param xmin Abcisse inférieure gauche, dans [-X, X], par défaut [-1, 1].
@@ -73,17 +72,17 @@ public class Functions {
    * @param ymax Ordonnée supérieure droite, dans [-Y, Y], par défaut [-1, 1].
    * @param c Numéro de la courbe: 0 (noir, défaut), 1 (brun), 2 (rouge), 3 (orange), 4 (jaune), 5 (vert), 6 (bleu), 7 (violet), 8 (gris), 9 (blanc).
    */
-  public static void scopeAddRectangle(double xmin, double ymin, double xmax, double ymax, int c) {
+  public static void addRectangle(double xmin, double ymin, double xmax, double ymax, int c) {
     scopeAddLine(xmin, ymin, xmax, ymin, c);
     scopeAddLine(xmax, ymin, xmax, ymax, c);
     scopeAddLine(xmax, ymax, xmin, ymax, c);
     scopeAddLine(xmin, ymax, xmin, ymin, c);
   }
   /*
-   * @see #scopeAddRectangle(double, double, double, double, int)
+   * @see #addRectangle(double, double, double, double, int)
    */
-  public static void scopeAddRectangle(double xmin, double ymin, double xmax, double ymax) {
-    scopeAddRectangle(xmin, ymin, xmax, ymax, 0);
+  public static void addRectangle(double xmin, double ymin, double xmax, double ymax) {
+    addRectangle(xmin, ymin, xmax, ymax, 0);
   }
   /** Trace une ligne.
    * @param x1 Abcisse du 1er point, dans [-X, X], par défaut [-1, 1].
@@ -92,14 +91,14 @@ public class Functions {
    * @param y2 Ordonnée du 2eme point, dans [-Y, Y], par défaut [-1, 1].
    * @param c Numéro de la courbe: 0 (noir, défaut), 1 (brun), 2 (rouge), 3 (orange), 4 (jaune), 5 (vert), 6 (bleu), 7 (violet), 8 (gris), 9 (blanc).
    */
-  public static void scopeAddLine(double x1, double y1, double x2, double y2, int c) {
+  public static void addLine(double x1, double y1, double x2, double y2, int c) {
     getPane().scope.add(x1, y1, x2, y2, c);
   }
   /*
-   * @see #scopeAddLine(double, double, double, double, int)
+   * @see #addLine(double, double, double, double, int)
    */
-  public static void scopeAddLine(double x1, double y1, double x2, double y2) {
-    scopeAddLine(x1, y1, x2, y2, 0);
+  public static void addLine(double x1, double y1, double x2, double y2) {
+    addLine(x1, y1, x2, y2, 0);
   }
   /** Trace un  cercle.
    * @param x Abcisse du centre, dans [-X, X], par défaut [-1, 1].
@@ -107,21 +106,21 @@ public class Functions {
    * @param r Rayon du cercle.
    * @param c Numéro de la courbe: 0 (noir, défaut), 1 (brun), 2 (rouge), 3 (orange), 4 (jaune), 5 (vert), 6 (bleu), 7 (violet), 8 (gris), 9 (blanc).
    */
-  public static void scopeAddCircle(double x, double y, double r, int c) {
+  public static void addCircle(double x, double y, double r, int c) {
     getPane().scope.add(x, y, r, c);
   }
   /*
-   * @see #scopeAddCircle(double, double, double, int)
+   * @see #addCircle(double, double, double, int)
    */
-  public static void scopeAddCircle(double x, double y, double r) {
-    scopeAddCircle(x, y, r, 0);
+  public static void addCircle(double x, double y, double r) {
+    addCircle(x, y, r, 0);
   }
   /** Renvoie la valeur horizontale du réticule. */
-  public static double scopeX() {
+  public static double getX() {
     return getPane().inputX.getValue();
   }
   /** Renvoie la valeur verticale du réticule. */
-  public static double scopeY() {
+  public static double getY() {
     return getPane().inputY.getValue();
   }
 }
