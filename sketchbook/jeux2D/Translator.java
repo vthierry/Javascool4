772e61a6e651af9f4d373adf3565ccfe1aa7c4c5
guillaume.quest@gmail.com <guillaume.quest@gmail.com@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-08-26 15:38:34 +0000

    git-svn-id: http://javascool.googlecode.com/svn@697 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- sketchbook/jeux2D/Translator.java
+++ sketchbook/jeux2D/Translator.java
@@ -1,4 +1,4 @@
-package org.javascool.proglets.jeuxConverted;
+package org.javascool.proglets.jeux2D;
 
 import java.lang.String;
 
@@ -19,6 +19,7 @@ public class Translator extends org.javascool.core.Translator {
     code.replaceAll("\\(double\\)", "(Double)");
     code.replaceAll("((^|\n)([ \t]*)(?!((public|private|protected)([ \t]+)))([A-Za-z0-9_]+)([ ]+)([A-Za-z0-9_]+)\\(([^()]*)\\)([ ]*)\\{([ \t]*)(\n|$))", "public $1");
     code.replaceAll("(^|[\n\t ])for[\n\t ]*\\(([A-Za-z0-9_.]+)[\n\t ]+([A-Za-z0-9_.]+)[\n\t ]+in[\n\t ]+([A-Za-z0-9_.]+)[\n\t ]*\\)[\n\t ]*\\{", "for (int tmpsystemi=0; tmpsystemi<$4.size(); tmpsystemi++) {$2 $3=($2)($4.get(tmpsystemi));");
-    return code;
+    System.out.println("TRANSLATING !!!");
+    return code+"//TRANSLATED";
   }
 }
