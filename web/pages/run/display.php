312fa212d6edd9826f6df8a95d60f97e584e8a90
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-08-26 08:04:49 +0000

    une raison inconnue
    git-svn-id: http://javascool.googlecode.com/svn@650 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- web/pages/run/display.php
+++ web/pages/run/display.php
@@ -11,23 +11,23 @@
 ?>
 
 <div class="display">
-    <p>Pour lancer Java's Cool sans installation, cliquez sur un des liens ci-dessus. Si le lancement échoue, vous devrez probablement installer Java. En cas d'échec voir la section "Plus d'informations" en bas de cette page.</p>
+    <p>Pour lancer Java's Cool sans installation, cliquez sur un des liens ci-dessus. Si le lancement échoue, vous devrez probablement installer Java (version au moins 1.6). 
+En cas d'échec voir la section "Plus d'informations" en bas de cette page.</p>
 
     <div class="label2">Windows</div>
-<p>Téléchargez Java's Cool au format .exe <a href="javascool.exe">ici</a></p>
+<p>Téléchargez Java's Cool au format .exe <a href="javascool-proglets.exe">ici</a> (ou au format jar exécutable <a href="javascool-proglets.jar">ici</a>).</p>
     <div class="label2">Mac OS</div>
-<p>Téléchargez Java's Cool au format .app <a href="javascool.app">ici</a></p>
+<p>Téléchargez Java's Cool au format .app <a href="javascool-proglets.app">ici</a> (ou au format jar exécutable <a href="javascool-proglets.jar">ici</a>).</p>
     <div class="label2">Linux</div>
-	<p>Sous linux, téléchargez Java's Cool au format .jar <a href="javascool.jar">ici</a>, puis suivez les instructions ci-dessous : 
+	<p>Sous linux, téléchargez Java's Cool au format .jar <a href="javascool-proglets.jar">ici</a>, puis suivez les instructions ci-dessous : 
     <table><tr><td><img src="images/screen1.png" alt="screenshot" style="width: 300px; height: auto;"/></td><td>
                 <span class="label-arrow" /></td><td>
                 <img src="images/screen2.png" alt="screenshot" style="width: 300px; height: auto;"/></td></tr></table></p>
                 
 <p>-On peut aussi choisir <tt>Sauver sous</tt> pour ne pas télécharger le logiciel à chaque lancement puis <tt>Ouvrir avec . . <i>java</i></tt>.</p>
-<p>-On peut aussi lancer à partir d'une ligne de commande <tt>java -jar javascool.jar</tt> une fois le logiciel téléchargé.</p>
+<p>-On peut aussi lancer à partir d'une ligne de commande <tt>java -jar javascool-proglets.jar</tt> une fois le logiciel téléchargé.</p>
     
 
 <div class="label2">Plus d'informations</div>
     <?php echo wiki_get_contents('JavaScool:Lancement'); ?>
-    <p>Vous pouvez aussi télécharger Java's Cool au format jar exécutable <a href="javascool.jar">ici</a></p>
 </div>
