f4d5eec6af4e93c3b9f590995d5b65a0e51ee9f1
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-10-06 09:26:38 +0000

    git-svn-id: http://javascool.googlecode.com/svn@904 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- work/lib/javascool.bat
+++ work/lib/javascool.bat
@@ -1,4 +1,4 @@
 rem Fichier de lancement de JavaScool 
-rem   pour recuperer les erreurs dans une fenetre utilisateur
+rem   pour recuperer les infos/erreurs dans une fenetre utilisateur
 "C:\Program Files\Java\jre6\bin\java" -jar javascool-proglets.jar
 pause
