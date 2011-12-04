28a5cb307e5b280133cc4182fc5d38a01693c426
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2011-12-04 10:28:16 +0000

    git-svn-id: http://javascool.googlecode.com/svn@968 17e3714e-118d-3e7a-01ef-bee73ecf6174

---
+++ sketchbook/grafikformate/Title.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Title extends Canvas{
+
+	private String title;
+	
+	public Title(String _title){
+		title = _title;
+	//	this.setBackground(Color.green);
+	}
+
+	public void paint(Graphics g){
+		g.drawString(title, 10, 15);
+	}
+
+}
