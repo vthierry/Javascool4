86172d45423fbf721231f4b0518f4b5e7077e831
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2012-02-04 17:28:11 +0000

    git-svn-id: http://javascool.googlecode.com/svn@1052 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- sketchbook/paintBrush/Functions.java
+++ sketchbook/paintBrush/Functions.java
@@ -21,17 +21,30 @@ public class Functions {
   /** Renvoie l'instance de la proglet pour accéder à ses éléments. */
   private static Panel getPane() {
     return Macros.getProgletPane();
-  }
+  } 
+  /** Remet l'affichage sur la proglet. */
+  static public void reset() {
+    org.javascool.gui.Desktop.getInstance().focusOnProgletPanel();
+  }  
   /** Définit le mécanisme de dessin de l'image à partir d'un codage de l'utilisateur.
    * @param manipImage L'implémentation de dessin à utiliser.
    */
   public static void setManipImage(PaintBrushManipImage manipImage) {
     getPane().setManipImage(manipImage);
   }
+  /** Change la valeur d'un pixel de l'image.
+   * @param x Abcisse de l'image, entre 0 et 31 inclut.
+   * @param y Ordonnée de l'image, entre 0 et 31 inclut.
+   * @return La couleur, entre 0 pour la couleur noire et 15 pour la couleur blanche (notée F en format hexadécimal), les autres valeurs correspondant à la palette de couleur.
+   */
   public static int getPixel(int x, int y) {
     return PaintBrushImage.getPixel(x, y);
   };
-
+  /** Change la valeur d'un pixel de l'image.
+   * @param x Abcisse de l'image, entre 0 et 31 inclut.
+   * @param y Ordonnée de l'image, entre 0 et 31 inclut.
+   * @param col Couleur, entre 0 pour la couleur noire et 15 pour la couleur blanche (notée F en format hexadécimal), les autres valeurs correspondant à la palette de couleur.
+   */
   public static void setPixel(int x, int y, int col) {
     PaintBrushImage.setPixel(x, y, col);
     getPane().repaint(0, 0, getPane().getWidth(), getPane().getHeight());
