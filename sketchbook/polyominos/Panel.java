0a6f828cf6b9f0490cf1e4d2549a7484f76f8809
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2012-06-15 09:40:09 +0000

    git-svn-id: http://javascool.googlecode.com/svn@1151 17e3714e-118d-3e7a-01ef-bee73ecf6174

---
+++ sketchbook/polyominos/Panel.java
@@ -0,0 +1,18 @@
+/*
+ * Fichier Panel.java
+ * pour avoir un Panel de démonstration.
+ *
+ */
+package org.javascool.proglets.polyominos;
+
+import org.dnsalias.avon.polyominos.PolyominosJPanel;
+
+/**
+ *
+ * @author Lionel Avon
+ * @since 2012-06-14
+ */
+public class Panel extends PolyominosJPanel {
+    static final long serialVersionUID = 0;
+    
+}
--- sketchbook/polyominos/proglet.pml
+++ sketchbook/polyominos/proglet.pml
@@ -1,4 +1,3 @@
 title = "Environnement de manipulation de polyominos"
 author = "Lionel Avon <lionel.avon@laposte.net>"
 icon = "pentomino_F.png"
-href = "http://avon.dnsalias.org/2011-2012/isn"
