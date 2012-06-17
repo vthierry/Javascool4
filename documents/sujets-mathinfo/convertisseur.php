2426bb7b465de950cb394fe5266fb9f65b0b0928
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2012-06-17 14:54:37 +0000

    git-svn-id: http://javascool.googlecode.com/svn@1152 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- documents/sujets-mathinfo/convertisseur.php
+++ documents/sujets-mathinfo/convertisseur.php
@@ -53,9 +53,8 @@ text-align: center;
 <body>
 <h2>Convertisseur</h2>
 Cet utilitaire code et décode des caractères
-<a target="more" href="http://fr.wikipedia.org/wiki/ASCII">ASCII</a> ainsi que des caractères étendus selon le codage
-<a target="more" href="http://fr.wikipedia.org/wiki/Windows-1252">ANSI</a>.<br>
-Le code ASCII proprement dit ne couvre que les caractères de numéros 0 à 127.<br />
+<a target="_parent" href="http://fr.wikipedia.org/wiki/ASCII">ASCII</a> ainsi que des caractères étendus selon le codage
+<a target="_parent" href="http://fr.wikipedia.org/wiki/Windows-1252">ANSI</a>. Le code ASCII proprement dit ne couvre que les caractères de numéros 0 à 127.<br />
 
 <table border=0 cellspacing=8 cellpadding=0 align="center">
 <tr>
@@ -64,8 +63,8 @@ Le code ASCII proprement dit ne couvre que les caractères de numéros 0 à 127.
 <form method="POST" action="http://igmaths.infos.st/outils/convertisseur.php">
 <td align=center valign=top>
 
-<b>[ <a target="more" href="http://fr.wikipedia.org/wiki/Ascii">TEXTE</a> ]</b><br>
-<textarea cols=34 rows=10 wrap="virtual" name="ascii" class="ff"><?php
+<b>[ <a target="_parent" href="http://fr.wikipedia.org/wiki/Ascii">TEXTE</a> ]</b><br>
+<textarea cols=35 rows=10 wrap="virtual" name="ascii" class="ff"><?php
 
 set_magic_quotes_runtime(0);
 foreach($_POST as $key=>$val){ $$key = stripslashes($val); }
@@ -106,10 +105,33 @@ echo htmlentities($ascii);
 <input type="submit" class="btn" value="ENCODE">
 </td></form>
 
+<!--CHAR-->
+<form method="POST"><td align=center valign=top>
+
+<b>[ <a target="_parent" href="http://fr.wikipedia.org/wiki/ASCII">ASCII Décimal </a> ]</b><br>
+<textarea cols=34 rows=10 wrap="virtual" name="char" class="ff"><?php
+if($char != "") echo $char;
+else if($ascii != "") {
+for($i = 0; $i < strlen($ascii); $i = $i + 1)
+	{
+	$nb = ord(substr($ascii, $i, 1));
+	if($nb<100) 
+		echo "0".$nb." ";
+		else
+		echo $nb." ";
+}
+}
+?></textarea>
+<br>
+<input type="submit" class="btn" value="DÉCODE">
+</td></form>
+</tr>
+
+<tr>
 <!-- BINAIRE -->
 <form method="POST"><td align=center valign=top>
-<b>[ <a target="more" href="http://fr.wikipedia.org/wiki/Code_binaire"><acronym title="Binaire">BINAIRE</acronym></a> ]</b><br>
-<textarea cols=34 rows=10 wrap="virtual" name="binary" class="ff"><?php
+<b>[ <a target="_parent" href="http://fr.wikipedia.org/wiki/Code_binaire"><acronym title="Binaire">BINAIRE</acronym></a> ]</b><br>
+<textarea cols=35 rows=10 wrap="virtual" name="binary" class="ff"><?php
 
 if($binary != "") echo $binary;
 else if($ascii != "") {
@@ -129,28 +151,30 @@ echo " ".str_repeat("0", 8-strlen($val)).$val;
 <!-- HEXA -->
 <form method="POST"><td align=center valign=top>
 
-<b>[ <a target="more" href="http://fr.wikipedia.org/wiki/Hexad%C3%A9cimal"><acronym title="Hexadécimal">HEXA</acronym></a> ]</b><br>
-<textarea cols=34 rows=10 wrap="virtual" name="hex" class="ff"><?php
+<b>[ <a target="_parent" href="http://fr.wikipedia.org/wiki/Hexad%C3%A9cimal"><acronym title="Hexadécimal">HEXA</acronym></a> ]</b><br>
+<textarea cols=34 rows=10  wrap="virtual" name="hex" class="ff"><?php
 
 if($hex != "") echo $hex;
 else if($ascii != "") {
 $val = dechex(ord(substr($ascii, 0, 1)));
-echo str_repeat("0", 2-strlen($val)).$val;
+echo str_repeat("0", 2-strlen($val)).$val." ";
 for($i = 1; $i < strlen($ascii); $i = $i + 1) {
   $val = dechex(ord(substr($ascii, $i, 1)));
-  echo " ".str_repeat("0", 2-strlen($val)).$val;
+  echo " ".str_repeat("0", 2-strlen($val)).$val." ";
 }
 }
 ?></textarea>
 <br>
 <input type="submit" class="btn" value="DÉCODE">
-</td></form></tr>
+</td></form>
+</tr>
 
+<tr>
 <!--BASE 64-->
-<form method="POST"><tr><td align=center valign=top>
+<form method="POST"><td align=center valign=top>
 
-<b>[ <a target="more" href="http://fr.wikipedia.org/wiki/Base64">BASE64</a> ]</b><br>
-<textarea cols=34 rows=10 wrap="virtual" name="b64" class="ff"><?php
+<b>[ <a target="_parent" href="http://fr.wikipedia.org/wiki/Base64">BASE64</a> ]</b><br>
+<textarea cols=35 rows=10 wrap="virtual" name="b64" class="ff"><?php
 if($b64 != "") echo $b64;
 else if($ascii != "") {
 echo base64_encode($ascii);
@@ -161,31 +185,10 @@ echo base64_encode($ascii);
 <input type="submit" class="btn" value="DÉCODE">
 </td></form>
 
-<!--CHAR-->
-<form method="POST"><td align=center valign=top>
-
-<b>[ <a target="more" href="http://fr.wikipedia.org/wiki/ASCII">ASCII Décimal </a> ]</b><br>
-<textarea cols=34 rows=10 wrap="virtual" name="char" class="ff"><?php
-if($char != "") echo $char;
-else if($ascii != "") {
-for($i = 0; $i < strlen($ascii); $i = $i + 1)
-	{
-	$nb = ord(substr($ascii, $i, 1));
-	if($nb<100) 
-		echo "0".$nb." ";
-		else
-		echo $nb." ";
-}
-}
-?></textarea>
-<br>
-<input type="submit" class="btn" value="DÉCODE">
-</td></form>
-
 <td align=center valign=center width="40em">
 Le code-source originel de cette page est disponible auprès de Paul Schou à 
-<a target="more" href="http://home.paulschou.net/tools/xlate/source.php">cette adresse</a>.<br>
-L'idée initiale est due à Nick Ciske, voir <a target="more" href="http://nickciske.com/binary">http://nickciske.com/binary</a>.
+<a target="_parent" href="http://home.paulschou.net/tools/xlate/source.php">cette adresse</a>.<br>
+L'idée initiale est due à Nick Ciske, voir <a target="_parent" href="http://nickciske.com/binary">http://nickciske.com/binary</a>.
 </td></tr></table>
 </body>
 </html>
