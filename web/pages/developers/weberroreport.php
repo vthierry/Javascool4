401b279f1061ff4388750d3cb3cd2271b53f6af1
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2012-06-26 16:25:17 +0000

    git-svn-id: http://javascool.googlecode.com/svn@1155 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- web/pages/developers/weberroreport.php
+++ web/pages/developers/weberroreport.php
@@ -1,5 +1,5 @@
 <?php // Mécanisme de report d'erreur à travers le web
   //include('includes/mailto.php');
-  if (isset($_GET['weberroreport']))
-    mailto("mailto:javascool@googlegroups.com?subject=javascool-web-alert", "Report:\n".$_GET['weberroreport']);
+  //if (isset($_GET['weberroreport']))
+  //  mailto("mailto:javascool@googlegroups.com?subject=javascool-web-alert", "Report:\n".$_GET['weberroreport']);
 ?>
