f6c0910414e3b11a2f6c6d5bfa7a03d837fe44ab
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2012-08-31 07:26:58 +0000

    git-svn-id: http://javascool.googlecode.com/svn@1195 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- work/lib/jvs4tojvs5.sh
+++ work/lib/jvs4tojvs5.sh
@@ -1,6 +1,10 @@
 #
+# Note
+#   Lancer ce script à partir de make -C .. mig 
+#   Ne plus propager les sources java
+#
 # To do liste:
-#   Faire un diff avant de propager les source java
+#   propager les source java
 #   Les liens pointent vers des fichiers htm alors qu'ils sont tous en html.
 #   Les fichiers sont enrobé de bootstrap, cela est inutile et impose un lourd calcule pour retrouver le vrai contenu de la page à afficher dans le WebPage.
 #
@@ -12,19 +16,24 @@ jvs="`pwd`/work/dist/javascool-builder.jar" ; jsx="`pwd`/work/lib/saxon.jar" ; x
 
 for p4 in sketchbook/* ; do p="`echo $p4 | sed 's/sketchbook\///'`" ; if [ "$p" != package.txt -a "$p" != sampleCode ] ; then p5="../jvs5/proglet-$p" 
 
-  # Copie en miroir des fichiers vers la cible
-  if [ \! -d "$p4" ] ; then echo "Erreur : la location source $p4 n'est pas un répertoire, bye." ; exit -1 ; fi
-  if [ \! -d "$p5" ] ; then mkdir -p $p5 ; fi
-  if [ -d ".svn" ] ; then svn -q export --force $p4 $p5/ ; else cp -r --force $p4 $p5 ; fi
+  echo "$p : $p4 -> /tmp/proglet-$p -> $p5"
 
-  echo "$p : $p4 -> $p5"
+  # Copie en miroir des fichiers vers le répertoire temporaire
+  if [ \! -d "$p4" ] ; then echo "Erreur : la location source $p4 n'est pas un répertoire, bye." ; exit -1 ; fi
+  /bin/rm -rf /tmp/proglet-$p ; mkdir -p /tmp/proglet-$p
+  if [ -d ".svn" ] ; then svn -q export --force $p4 /tmp/proglet-$p/ ; else cp -r --force $p4 /tmp/proglet-$p ; fi
 
   # Translation des fichiers
-  pushd $p5 > /dev/null
+  pushd /tmp/proglet-$p > /dev/null
   if [ -f proglet.pml ] ; then java -cp $jvs org.javascool.tools.Pml proglet.pml proglet.json ; rm proglet.pml ; else echo "Erreur : y'a pas de proglet.pml !" ; fi
   if [ -f completion.xml ] ; then java -cp $jvs org.javascool.tools.Pml completion.xml completion.json ; rm completion.xml ; fi
   for f in *.xml ; do n=`echo $f | sed s'/.xml$//'` ; java -jar $jsx -o $n.html $n.xml $xslt ; rm $f ; done
+  for f in *.html ; do cp $f $f~ ; sed s'/.htm"/.html"/g' < $f~ > $f ; done
   popd > /dev/null
+
+  # Transfert dans les proglets 
+  cp /tmp/proglet-$p/*.html /tmp/proglet-$p/*.jvs $p5
+  pushd $p5  > /dev/null ; git status ; popd > /dev/null
 fi done
 
 # That's all !
