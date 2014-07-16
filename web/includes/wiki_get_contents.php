fa5fc5b2f81031c3d5efb7cf3738843b880c61c2
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2014-07-16 15:32:18 +0000

    git-svn-id: http://javascool.googlecode.com/svn@1404 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- web/includes/wiki_get_contents.php
+++ web/includes/wiki_get_contents.php
@@ -3,7 +3,7 @@
 // Renvoie le texte sur http://wiki.inria.fr/sciencinfolycee avec mise en forme des liens
 function wiki_get_contents($name) {
   // return cache_get_contents_exists($name) ? cache_get_contents_get($name) : cache_get_contents_put($name, wiki_get_contents_load($name));
-  echo "<hr>/home/groups/javascool/htdocs/v4/cache/".$name."<hr><tt>Désolé le site est cassé suite à une erreur des services informatiques <a href='http://dsi.inria.fr'>http://dsi.inria.fr</a> Inria</tt><hr>";
+  echo "<hr>/home/groups/javascool/htdocs/v4/cache/".$name."<hr><tt>Désolé le site est cassé suite à une erreur des services informatiques Inria</tt><hr>";
   /*
   $txt0 = file_get_contents('/home/groups/javascool/htdocs/v4/cache/'+$name, FILE_USE_INCLUDE_PATH);
   $txt1 = file_get_contents('./cache/'+$name, FILE_USE_INCLUDE_PATH);
