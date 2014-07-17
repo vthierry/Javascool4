26a276f5d01afe2238fbabbbc226c9283c20d637
Thierry.Vieville@sophia.inria.fr <Thierry.Vieville@sophia.inria.fr@17e3714e-118d-3e7a-01ef-bee73ecf6174>
2014-07-17 12:50:30 +0000

    git-svn-id: http://javascool.googlecode.com/svn@1405 17e3714e-118d-3e7a-01ef-bee73ecf6174

--- web/includes/wiki_get_contents.php
+++ web/includes/wiki_get_contents.php
@@ -3,7 +3,11 @@
 // Renvoie le texte sur http://wiki.inria.fr/sciencinfolycee avec mise en forme des liens
 function wiki_get_contents($name) {
   // return cache_get_contents_exists($name) ? cache_get_contents_get($name) : cache_get_contents_put($name, wiki_get_contents_load($name));
-  echo "<hr>/home/groups/javascool/htdocs/v4/cache/".$name."<hr><tt>Désolé le site est cassé suite à une erreur des services informatiques Inria</tt><hr>";
+  $text = file_get_contents('v4/cache/'+$name, FILE_USE_INCLUDE_PATH);
+  if ($text === FALSE)
+    echo "<hr>/home/groups/javascool/htdocs/v4/cache/".$name."<hr><tt>Désolé le site est cassé suite à une erreur des services informatiques Inria</tt><hr>";
+  else
+    return $text;
   /*
   $txt0 = file_get_contents('/home/groups/javascool/htdocs/v4/cache/'+$name, FILE_USE_INCLUDE_PATH);
   $txt1 = file_get_contents('./cache/'+$name, FILE_USE_INCLUDE_PATH);
@@ -12,7 +16,6 @@ function wiki_get_contents($name) {
   if ($txt1 === FALSE) echo "<br>KK1 :".file_exists('./cache/'+$name)."!";
   if ($txt2 === FALSE) echo "<br>KK2 :".file_exists('./v4/cache/'+$name)."!";
   */
-  return file_get_contents('/home/groups/javascool/htdocs/v4/cache/'+$name, FILE_USE_INCLUDE_PATH);
 }
 
 ?>
